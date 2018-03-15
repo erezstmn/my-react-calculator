2343fa8a18ef5ee4a01b430679dac75bd4463f6f
erezstmn <erezstmn@gmail.com>
2018-03-15 16:44:34 +0200

    more funcs

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Textfit } from 'react-textfit';
+import NumberFormat from 'react-number-format';
 import './App.css';
 
 
@@ -12,39 +14,61 @@ class App extends Component {
     this.handleEqualClick = this.handleEqualClick.bind(this);  
     this.handleDecimalClick = this.handleDecimalClick.bind(this);
     this.handleOperatorClick = this.handleOperatorClick.bind(this);
+    this.handleOnMouseDown = this.handleOnMouseDown.bind(this);
+    this.handleOnMouseUp = this.handleOnMouseUp.bind(this); 
+    this.handleKeyPress = this.handleKeyPress.bind(this);
     this.state= {
       newCalculation:true,
       numberToRender:"0",
       leftOperand:null,
       operator:null,
       isDecimalAvailable: true,
-      resultClass: "result-big"
+      resultClass: "result-big",
+      lightgrat:"lightgray",
+      acButton:"AC"
     }
   }  
   // componentWillUpdate(nextProps, nextState){
   //   console.log(nextState);
-  // }
+  // }+
+  componentWillMount(){
+    document.addEventListener("keypress", this.handleKeyPress);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keypress", this.handleKeyPress);
+}
   handleNClick(e){
     let num = e.target.value;
     this.setState((prevState) => {
       let newNumber = (this.state.numberToRender==="0" || this.state.newCalculation) ? num : prevState.numberToRender + num;
       return {
         numberToRender: newNumber,
-        newCalculation: false
+        newCalculation: false,
+        acButton:"C"
       };
     })
   }
-  handleACClick(){
-    this.setState(() =>{
-      return {
-        newCalculation: true,
-        numberToRender: "0",
-        leftOperand: null,
-        operator: null,
-        isDecimalAvailable: true,
+  handleACClick(e){
+    if(e.target.value==="AC"){
+      this.setState(() =>{
+        return {
+          newCalculation: true,
+          numberToRender: "0",
+          leftOperand: null,
+          operator: null,
+          isDecimalAvailable: true,
 
-      };
-    })
+        };
+      })
+    }
+    if(e.target.value==="C"){
+      this.setState(() =>{
+        return {
+          numberToRender: "0",
+          acButton:"AC"
+        };
+      })
+    }
   }
   handlePlusMinusClick(){  
     let number = parseFloat(this.state.numberToRender,10);
@@ -62,7 +86,8 @@ class App extends Component {
         return {
           numberToRender: newNumber,
           newCalculation: false,
-          isDecimalAvailable: false
+          isDecimalAvailable: false,
+          acButton:"C"
         };
       } else{
         return;
@@ -95,7 +120,8 @@ class App extends Component {
         newCalculation: true,        
         numberToRender: result,
         operator:null,
-        isDecimalAvailable: true
+        isDecimalAvailable: true,
+        acButton:"C"
       };
     })
   }
@@ -105,44 +131,88 @@ class App extends Component {
       if (operator==="%"){
         return {
           isDecimalAvailable: false,
-          numberToRender: parseFloat(prevState.numberToRender)/100
+          numberToRender: parseFloat(prevState.numberToRender)/100          
         }
       }else{      
         return{
           operator: operator,
           newCalculation: true,
           isDecimalAvailable: true,
-          leftOperand: prevState.numberToRender
+          leftOperand: prevState.numberToRender,
+          acButton:"C"
         };
       }
     })
   }
+  handleOnMouseDown(e){   
+    switch(e.target.className){
+      case "App-ButtonNumbers":
+      case "App-ZeroButton":
+        e.target.style='background-color:silver;';
+        break;
+      case "App-ButtonOperators":
+        e.target.style='background-color:RGB(207, 145, 39);';
+        break;
+      case "App-ButtonFirstRow":
+        e.target.style='background-color:RGB(165, 163, 163);';
+        break;
+      default:
+        break;
+    }
+  }
+  handleOnMouseUp(e){    
+    switch(e.target.className){
+      case "App-ButtonNumbers":
+      case "App-ZeroButton":
+        e.target.style='background-color:lightgray;';
+        break;
+      case "App-ButtonOperators":
+        e.target.style='background-color:orange;';
+        break;
+        case "App-ButtonFirstRow":
+        e.target.style='background-color:silver;';
+        break;
+      default:
+        break;
+    }
+  }
+  handleKeyPress(e){
+    if (parseInt(e.key,10) || e.key==="0"){
+        let object ={
+          target:{
+            value: e.key
+          }
+        }  
+      this.handleNClick(object);
+    }    
+  }    
+
   render() {
     return (
       <div className="App">
-        <p className="result">{this.state.numberToRender}</p>        
-        <button className="App-ButtonFirstRow" onClick={this.handleACClick}>AC</button>
-        <button className="App-ButtonFirstRow" onClick={this.handlePlusMinusClick}>+-</button>
-        <button className="App-ButtonFirstRow" value="%" onClick={this.handleOperatorClick}>%</button>
-        <button className="app-ButtonOperators" value="/" onClick={this.handleOperatorClick}>/</button><br/>
-        <button className="app-ButtonNumbers" value="7" onClick={this.handleNClick}>7</button>
-        <button className="app-ButtonNumbers" value="8" onClick={this.handleNClick}>8</button>
-        <button className="app-ButtonNumbers" value="9" onClick={this.handleNClick}>9</button>
-        <button className="app-ButtonOperators" value="*" onClick={this.handleOperatorClick}>*</button><br/>
-        <button className="app-ButtonNumbers" value="4" onClick={this.handleNClick}>4</button>
-        <button className="app-ButtonNumbers" value="5" onClick={this.handleNClick}>5</button>
-        <button className="app-ButtonNumbers" value="6" onClick={this.handleNClick}>6</button>
-        <button className="app-ButtonOperators" value="-" onClick={this.handleOperatorClick}>-</button><br/>
-        <button className="app-ButtonNumbers" value="1" onClick={this.handleNClick}>1</button>
-        <button className="app-ButtonNumbers" value="2" onClick={this.handleNClick}>2</button>
-        <button className="app-ButtonNumbers" value="3" onClick={this.handleNClick}>3</button>
-        <button className="app-ButtonOperators" value="+" onClick={this.handleOperatorClick}>+</button><br/>
-        <button className="App-Button1" value="0" onClick={this.handleNClick}>0</button>
-        <button className="app-ButtonNumbers" onClick={this.handleDecimalClick}>.</button>
-        <button className="app-ButtonOperators" onClick={this.handleEqualClick}>=</button>               
+        <Textfit mode="single" max="70" className="result"><NumberFormat value={this.state.numberToRender} displayType={'text'} thousandSeparator={true}/></Textfit>               
+        <button className="App-ButtonFirstRow" value={this.state.acButton} onClick={this.handleACClick} onMouseDown={this.handleOnMouseDown} onMouseUp={this.handleOnMouseUp}>{this.state.acButton}</button>
+        <button className="App-ButtonFirstRow" onClick={this.handlePlusMinusClick} onMouseDown={this.handleOnMouseDown} onMouseUp={this.handleOnMouseUp}>+-</button>
+        <button className="App-ButtonFirstRow" value="%" onClick={this.handleOperatorClick} onMouseDown={this.handleOnMouseDown} onMouseUp={this.handleOnMouseUp}>%</button>
+        <button className="App-ButtonOperators" value="/" onClick={this.handleOperatorClick} onMouseDown={this.handleOnMouseDown} onMouseUp={this.handleOnMouseUp}>/</button><br/>
+        <button className="App-ButtonNumbers" value="7" onClick={this.handleNClick} onMouseDown={this.handleOnMouseDown} onMouseUp={this.handleOnMouseUp}>7</button>
+        <button className="App-ButtonNumbers" value="8" onClick={this.handleNClick} onMouseDown={this.handleOnMouseDown} onMouseUp={this.handleOnMouseUp}>8</button>
+        <button className="App-ButtonNumbers" value="9" onClick={this.handleNClick} onMouseDown={this.handleOnMouseDown} onMouseUp={this.handleOnMouseUp}>9</button>
+        <button className="App-ButtonOperators" value="*" onClick={this.handleOperatorClick} onMouseDown={this.handleOnMouseDown} onMouseUp={this.handleOnMouseUp}>X</button><br/>
+        <button className="App-ButtonNumbers" value="4" onClick={this.handleNClick} onMouseDown={this.handleOnMouseDown} onMouseUp={this.handleOnMouseUp}>4</button>
+        <button className="App-ButtonNumbers" value="5" onClick={this.handleNClick} onMouseDown={this.handleOnMouseDown} onMouseUp={this.handleOnMouseUp}>5</button>
+        <button className="App-ButtonNumbers" value="6" onClick={this.handleNClick} onMouseDown={this.handleOnMouseDown} onMouseUp={this.handleOnMouseUp}>6</button>
+        <button className="App-ButtonOperators" value="-" onClick={this.handleOperatorClick} onMouseDown={this.handleOnMouseDown} onMouseUp={this.handleOnMouseUp}>-</button><br/>
+        <button className="App-ButtonNumbers" value="1" onClick={this.handleNClick} onMouseDown={this.handleOnMouseDown} onMouseUp={this.handleOnMouseUp}>1</button>
+        <button className="App-ButtonNumbers" value="2" onClick={this.handleNClick} onMouseDown={this.handleOnMouseDown} onMouseUp={this.handleOnMouseUp}>2</button>
+        <button className="App-ButtonNumbers" value="3" onClick={this.handleNClick} onMouseDown={this.handleOnMouseDown} onMouseUp={this.handleOnMouseUp}>3</button>
+        <button className="App-ButtonOperators" value="+" onClick={this.handleOperatorClick} onMouseDown={this.handleOnMouseDown} onMouseUp={this.handleOnMouseUp}>+</button><br/>
+        <button className="App-ZeroButton" value="0" onClick={this.handleNClick} onMouseDown={this.handleOnMouseDown} onMouseUp={this.handleOnMouseUp}>0</button>
+        <button className="App-ButtonNumbers" onClick={this.handleDecimalClick} onMouseDown={this.handleOnMouseDown} onMouseUp={this.handleOnMouseUp}>.</button>
+        <button className="App-ButtonOperators" onClick={this.handleEqualClick} onMouseDown={this.handleOnMouseDown} onMouseUp={this.handleOnMouseUp}>=</button>               
       </div>
     );
-  }
+  } 
 }
 
 export default App;
